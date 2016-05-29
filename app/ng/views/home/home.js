f3565e77ba691239b0396a22b19c94b7b1dd088e
Simon Bönisch <simonboenisch@yahoo.de>
2016-05-29 23:40:04 +0200

    fixed routing

--- app/ng/views/home/home.js
+++ app/ng/views/home/home.js
@@ -2,8 +2,20 @@
 
 angular.module('foodGenerator.home', ['ngResource', 'ui.router'])
 
-    .controller('HomeCtrl', function($scope) {
-        $scope.greeting = "Welcome home!";
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('home', {
+                parent: 'root',
+                url: '/home',
+                views: {
+                    "content": {
+                        templateUrl: "views/home/home.html"
+                    }
+                }
+            })
     })
 
+    .controller('HomeCtrl', function ($scope) {
+        $scope.greeting = "Welcome home!";
+    })
 ;
