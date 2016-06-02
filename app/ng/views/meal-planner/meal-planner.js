09431441b4623d0adb322d9442e5be051ed2988f
Simon Bönisch <simonboenisch@yahoo.de>
2016-06-03 00:11:33 +0200

    added skeletons for meal planner and ingredient search

---
+++ app/ng/views/meal-planner/meal-planner.js
@@ -0,0 +1,20 @@
+angular.module('foodGenerator.mealPlanner', ['ngResource', 'ui.router'])
+
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('mealPlanner', {
+                parent: 'root',
+                url: '/meal-planner',
+                views: {
+                    "content": {
+                        templateUrl: "views/meal-planner/meal-planner.html",
+                        controller: 'MealPlannerCtrl'
+                    }
+                }
+            })
+    })
+
+    .controller('MealPlannerCtrl', function ($scope) {
+        $scope.exampleField = "Hello World!";
+    })
+;
