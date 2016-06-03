fbec7e9031d05d0b8c84b04ef1501881ba637ce3
Simon Bönisch <simonboenisch@yahoo.de>
2016-06-04 01:48:03 +0200

    removed logging outputs

--- app/ng/components/login-dialog/login-ctrl.js
+++ app/ng/components/login-dialog/login-ctrl.js
@@ -6,16 +6,12 @@ angular.module('foodGenerator')
         $scope.cancel = cancel;
 
         function login() {
-            console.log("Performing login with email '" + $scope.email + "' and password '" + $scope.password + "'.");
             currentUser.login($scope.email, $scope.password).then(function () {
-                console.log("closing model. (success?)");
                 $uibModalInstance.close();
             }, function (response) {
                 if (response.status == 400 || response.status == 401) {
-                    console.log("wrong username and pw");
                     $scope.errorText = "Wrong username or password.";
                 } else {
-                    console.log("unkown error");
                     $scope.errorText = "An unknown error occured. Please try again later.";
                 }
             });
