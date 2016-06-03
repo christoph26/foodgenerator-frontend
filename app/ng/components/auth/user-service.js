e99f94aefce4ce8ee4fb5a56b6a7273f0abcf3e8
Simon Bönisch <simonboenisch@yahoo.de>
2016-06-04 00:56:32 +0200

    copied and adopted user service from seba example project

---
+++ app/ng/components/auth/user-service.js
@@ -0,0 +1,37 @@
+(function () {
+
+    angular.module('foodGenerator')
+        .service('currentUser', currentUserService);
+
+    function currentUserService(BASEURL, $http, auth) {
+
+        this.register = register;
+        this.login = login;
+        this.loggedIn = auth.isAuthed;
+        this.logout = auth.deleteToken;
+        this.getUser = getUser;
+
+
+        ////////////////
+
+        function register(user, pass) {
+            return $http.post(BASEURL + '/signup', {
+                username: user,
+                password: pass
+            });
+        }
+
+        function login(user, pass) {
+            return $http.post(BASEURL + '/login', {
+                username: user,
+                password: pass
+            });
+        }
+
+        function getUser() {
+            var token = auth.getToken();
+            return token ? auth.parseJwt(token).user : {};
+        }
+    }
+
+})();
