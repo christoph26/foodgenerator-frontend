98fc07523a52b6b63ce60d1aa08f580fd8fb6853
Simon Bönisch <simonboenisch@yahoo.de>
2016-06-05 15:00:05 +0200

    made account page reload automatically

--- app/ng/views/account/account.js
+++ app/ng/views/account/account.js
@@ -14,7 +14,17 @@ angular.module('foodGenerator.account', ['ngResource', 'ui.router'])
             })
     })
 
-    .controller('AccountCtrl', function ($scope, auth) {
-        $scope.isLoggedIn = auth.isAuthed;
+    .controller('AccountCtrl', function ($scope, $state, currentUser) {
+        var loggedIn = currentUser.loggedIn();
+        $scope.$watch(function () {
+            return currentUser.loggedIn();
+        }, function (isLoggedIn) {
+            if (loggedIn != isLoggedIn) {
+                // check if state really changed to avoid endless reloading
+                loggedIn = isLoggedIn;
+                $scope.loggedIn = isLoggedIn;
+                $state.go($state.current.name, $state.params, {reload: true});
+            }
+        });
     })
 ;
