bb18b1a0c1bbf09482d57b0637e2b94e504b10b6
Simon Bönisch <simonboenisch@yahoo.de>
2016-06-16 12:02:52 +0200

    added meal label js and integrated component into meal planner

---
+++ app/ng/components/meal-label/meal-label.js
@@ -0,0 +1,11 @@
+angular.module('foodGenerator')
+    .directive('meallabel', function () {
+        return {
+            restrict: "E",
+            templateUrl: "components/meal-label/meal-label.html",
+            scope: {
+                recipe: '='
+            }
+        }
+    })
+;
