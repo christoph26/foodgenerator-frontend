557965a46f327b11032a605bb56a20c22db2fa7e
Simon Bönisch <simonboenisch@yahoo.de>
2016-06-19 19:44:57 +0200

    implemented update of angular js backend entities

--- app/ng/components/meal-list/meal-list.js
+++ app/ng/components/meal-list/meal-list.js
@@ -3,9 +3,20 @@ angular.module('foodGenerator')
         return {
             restrict: "E",
             templateUrl: "components/meal-list/meal-list.html",
+            controller: "MealListCtrl",
             scope: {
                 mealList: '='
             }
+        }
+    })
+
+    .controller("MealListCtrl", function ($scope) {
+        $scope.updateMealOrder = function () {
+            // update order numbers after inserting element and reordering list
+            for (var index in $scope.mealList.meals) {
+                $scope.mealList.meals[index].order = index;
+            }
+            console.log($scope.mealList.meals);
         }
     })
 ;
