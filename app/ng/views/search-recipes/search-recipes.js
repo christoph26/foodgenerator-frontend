070dcc0d3c7b3907d452e758b63be0c5b232d395
Simon Bönisch <simonboenisch@yahoo.de>
2016-06-19 15:34:17 +0200

    fixed error message "resultsList is not a function"; removed console output; fixed invalid html closing tags; auto-formatted

--- app/ng/views/search-recipes/search-recipes.js
+++ app/ng/views/search-recipes/search-recipes.js
@@ -18,12 +18,10 @@ angular.module('foodGenerator.search.recipes', ['ngResource', 'ui.router'])
             $scope.searchTerm = SearchService.searchTerm;
 
             SearchService.performRecipeSearch().then(function (response) {
-                console.log(response.data);
                 $scope.resultsList = response.data;
                 $scope.searchTerm = SearchService.searchTerm;
-            }, function (error) {
-                console.log(error.data);
-                $scope.resultsList([]);
+            }, function () {
+                $scope.resultsList = [];
                 $scope.searchTerm = SearchService.searchTerm;
             });
         }
