0564970acdec87a12e4dea87875676f72dc60d47
Stefan <stefanbock@mytum.de>
2016-06-22 15:22:27 +0200

    add missing ingredients at thumbnail

--- app/ng/components/app.js
+++ app/ng/components/app.js
@@ -10,7 +10,7 @@ angular.module('foodGenerator', [
     'foodGenerator.search.ingredients',
     'foodGenerator.search.recipes',
     'foodGenerator.mealPlanner',
-    'foodGenerator.recipe',
+    'foodGenerator.recipeDetail',
     'templates',
     'ngTagsInput'
 ])
--- app/ng/views/recipe-details/recipe-details.js
+++ app/ng/views/recipe-details/recipe-details.js
@@ -1,10 +1,10 @@
-angular.module('foodGenerator.recipe', ['ngResource', 'ui.router'])
+angular.module('foodGenerator.recipeDetail', ['ngResource', 'ui.router'])
 
     .config(function ($stateProvider) {
         $stateProvider
-            .state('recipe', {
+            .state('recipeDetail', {
                 parent: 'root',
-                url: '/recipe',
+                url: '/recipeDetail',
                 views: {
                     "content": {
                         templateUrl: "views/recipe-details/recipe-details.html",
@@ -14,7 +14,79 @@ angular.module('foodGenerator.recipe', ['ngResource', 'ui.router'])
             })
     })
 
-    .controller('RecipeCtrl', function ($scope) {
-        //TODO: do something in cooperation with searchservice if "search ingredients" is clicked
+    .controller('RecipeDetailCtrl', function ($scope) {
+        $scope.recipe =
+
+        {
+            "_id": "333300000000000000000001",
+            "title": "Spaghetti Bolognese Classic",
+            "effort": 1,
+            "description": "Preperation is simple and done in 30 minutes.",
+            "vegan": false,
+            "vegetarian": false,
+            "picture": "img/recipes/spaghetti-bolognese-classic.jpg",
+            "ingredientList": [
+                {
+                    "_id": "111100000000000000000001",
+                    "title": "Minced Meat",
+                    "supermarkets": [
+                        "888800000000000000000001",
+                        "888800000000000000000002",
+                        "888800000000000000000003"
+                    ]
+                },
+                {
+                    "_id": "111100000000000000000002",
+                    "title": "Tomato",
+                    "supermarkets": [
+                        "888800000000000000000001",
+                        "888800000000000000000002",
+                        "888800000000000000000003"
+                    ]
+                },
+                {
+                    "_id": "111100000000000000000003",
+                    "title": "Onion",
+                    "supermarkets": [
+                        "888800000000000000000001",
+                        "888800000000000000000002",
+                        "888800000000000000000003"
+                    ]
+                },
+                {
+                    "_id": "111100000000000000000004",
+                    "title": "Garlic",
+                    "supermarkets": [
+                        "888800000000000000000001",
+                        "888800000000000000000002",
+                        "888800000000000000000003"
+                    ]
+                },
+                {
+                    "_id": "111100000000000000000005",
+                    "title": "Oregano",
+                    "supermarkets": [
+                        "888800000000000000000003"
+                    ]
+                },
+                {
+                    "_id": "111100000000000000000006",
+                    "title": "Spaghetti",
+                    "supermarkets": [
+                        "888800000000000000000001",
+                        "888800000000000000000002",
+                        "888800000000000000000003"
+                    ]
+                }
+            ],
+            "recipeFamily": "444400000000000000000001",
+            "availability": [
+                {
+                    "_id": "888800000000000000000003",
+                    "title": "REAL",
+                    "icon": "http://static05.kaufda.de/Geschaefte/Real.v1111.png"
+                }
+            ]
+        }
     })
 ;
