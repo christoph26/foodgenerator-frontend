3a65ef1c121f0dc0293ae3d9d61a6f62ccd5f850
Simon Bönisch <simonboenisch@yahoo.de>
2016-06-28 16:31:43 +0200

    reordered, removed example meal plan

--- app/ng/views/meal-planner/meal-planner.js
+++ app/ng/views/meal-planner/meal-planner.js
@@ -15,11 +15,19 @@ angular.module('foodGenerator.mealPlanner', ['ngResource', 'ui.router', 'dndList
     })
 
     .controller('MealPlannerCtrl', function ($scope, $filter, $uibModal, mealPlanService) {
-        $scope.mealPlan = exampleMealPlan;
+        $scope.mealPlan = getEmptyMealPlan();
+        addMealList();
+
         $scope.markedRecipes = markedExampleRecipes;
         $scope.recentRecipes = viewedExampleRecipes;
 
-        $scope.addMealList = function () {
+        $scope.open = open;
+        $scope.saveMealPlan = saveMealPlan;
+        $scope.addMealList = addMealList;
+
+        //////////////////////////////////
+
+        function addMealList() {
             var mealPlan = $scope.mealPlan;
             var newMealList = {
                 title: calculateMealListTitle(mealPlan, $filter),
@@ -27,9 +35,9 @@ angular.module('foodGenerator.mealPlanner', ['ngResource', 'ui.router', 'dndList
                 meals: []
             };
             mealPlan.mealLists.push(newMealList);
-        };
+        }
 
-        $scope.open = function (type) {
+        function open(type) {
             var modalInstance;
             if (type == 'load') {
                 modalInstance = $uibModal.open({
@@ -63,44 +71,51 @@ angular.module('foodGenerator.mealPlanner', ['ngResource', 'ui.router', 'dndList
                     $scope.mealPlan = updatedMealPlan;
                 });
             }
-        };
+        }
 
-        $scope.saveMealPlan = function () {
+        function saveMealPlan() {
             if ($scope.mealPlan._id) {
                 mealPlanService.update($scope.mealPlan);
             } else {
                 $scope.open('save');
             }
-        };
+        }
+
+        function getEmptyMealPlan() {
+            return {
+                title: undefined,
+                mealLists: []
+            };
+        }
+
+        function calculateMealListTitle(mealPlan, $filter) {
+            var mealLists = mealPlan.mealLists;
+            var date = new Date();
+            if (mealLists && mealLists.length) {
+                // if the last item has a date as caption, increment it by one day
+                var lastTitle = mealLists[mealLists.length - 1].title;
+                if (parseDate(lastTitle)) {
+                    date = parseDate(lastTitle);
+                    date.setDate(date.getDate() + 1);
+                }
+            }
+            return $filter('date')(date, 'dd.MM.yyyy');
+        }
+
+        function parseDate(dateString) {
+            var date = dateString.split(".");
+            if (date.length == 3) {
+                var day = date[0];
+                var month = date[1];
+                var year = date[2];
+                var dateStringISO = year + "-" + month + "-" + day;
+                return new Date(dateStringISO);
+            }
+            return undefined;
+        }
 
     })
 ;
-
-function calculateMealListTitle(mealPlan, $filter) {
-    var mealLists = mealPlan.mealLists;
-    var date = new Date();
-    if (mealLists && mealLists.length) {
-        // if the last item has a date as caption, increment it by one day
-        var lastTitle = mealLists[mealLists.length - 1].title;
-        if (parseDate(lastTitle)) {
-            date = parseDate(lastTitle);
-            date.setDate(date.getDate() + 1);
-        }
-    }
-    return $filter('date')(date, 'dd.MM.yyyy');
-}
-
-function parseDate(dateString) {
-    var date = dateString.split(".");
-    if (date.length == 3) {
-        var day = date[0];
-        var month = date[1];
-        var year = date[2];
-        var dateStringISO = year + "-" + month + "-" + day;
-        return new Date(dateStringISO);
-    }
-    return undefined;
-}
 
 var markedExampleRecipes = [
     {
@@ -162,55 +177,3 @@ var viewedExampleRecipes = [
         "recipeFamily": "444400000000000000000003"
     }
 ];
-
-var exampleMealPlan = {
-    title: "myTestPlan",
-    mealLists: [{
-        title: "24.06.2016",
-        order: 0,
-        meals: [{
-            order: 0,
-            recipe: {
-                "_id": "333300000000000000000005",
-                "title": "Avocado Salad Feta",
-                "effort": 1,
-                "description": "Delicious Healthy Salat for Vegetarian People",
-                "vegan": false,
-                "vegetarian": true,
-                "picture": "img/recipes/avocado-salad-feta.jpg",
-                "ingredientList": "222200000000000000000005",
-                "recipeFamily": "444400000000000000000003"
-            }
-        }, {
-            order: 1,
-            recipe: {
-                "_id": "333300000000000000000001",
-                "title": "Spaghetti Bolognese Classic",
-                "effort": 1,
-                "description": "Preperation is simple and done in 30 minutes.",
-                "vegan": false,
-                "vegetarian": false,
-                "picture": "img/recipes/spaghetti-bolognese-classic.jpg",
-                "ingredientList": "222200000000000000000001",
-                "recipeFamily": "444400000000000000000001"
-            }
-        }]
-    }, {
-        title: "25.06.2016",
-        order: 1,
-        meals: [{
-            order: 1,
-            recipe: {
-                "_id": "333300000000000000000003",
-                "title": "Chili Con Carne",
-                "effort": 1,
-                "description": "Hot & Spicy",
-                "vegan": false,
-                "vegetarian": false,
-                "picture": "img/recipes/chili-con-carne.jpg",
-                "ingredientList": "222200000000000000000003",
-                "recipeFamily": "444400000000000000000002"
-            }
-        }]
-    }]
-};
