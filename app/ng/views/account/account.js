9093546f638c9ed328d9f4b2b6fd1e94d9623427
Norbi <roberthaecker@web.de>
2016-06-29 21:41:56 +0200

    update 1.0 formular finished

--- app/ng/views/account/account.js
+++ app/ng/views/account/account.js
@@ -16,6 +16,62 @@ angular.module('foodGenerator.account', ['ngResource', 'ui.router'])
 
     .controller('AccountCtrl', function ($scope, $state, currentUser) {
         var loggedIn = currentUser.loggedIn();
+
+
+//pressing the button "Save Update"
+        $scope.toggle = function () {
+            console.log("123");
+            debugger;
+            var model = this;
+
+
+            debugger;
+            if (this.update !== undefined) {
+                if (this.update.firstName !== undefined) {
+                    if (this.update.firstName != "") {
+                        model.firstName = this.update.firstName;
+                    }
+                }
+                if (this.update.lastName !== undefined) {
+                    if (this.update.lastName != "") {
+                        model.lastName = this.update.lastName;
+                    }
+                }
+                debugger;
+                if (this.update.email !== undefined) {
+                    if (this.update.email != "") {
+
+                        model.email = this.update.email;
+                    }
+                }
+                if (this.update.password1 !== undefined) {
+                    if (this.update.password1 != "") {
+
+                        model.password = this.update.password1;
+                    }
+                }
+
+            }
+            
+
+            debugger;
+
+            model.submit = function (isValid) {
+                console.log("h");
+                if (isValid) {
+                    $http.put(BASEURL + '/users/' + user._id, user).then(function (response) {
+                        model.message = "updated";
+                    }, function (error) {
+                        // hier den fehlerfall behandeln
+                        model.message = "NOT updated";
+                    });
+
+                } else {
+                    model.message = "There are still invalid fields";
+                }
+            };
+        }
+//checks if the user is logged in
         $scope.$watch(function () {
             return currentUser.loggedIn();
         }, function (isLoggedIn) {
@@ -26,5 +82,25 @@ angular.module('foodGenerator.account', ['ngResource', 'ui.router'])
                 $state.go($state.current.name, $state.params, {reload: true});
             }
         });
+
+//compares the input to 2 text input boxes
+        var compareTo = function () {
+            return {
+                require: "ngModel",
+                scope: {
+                    otherModelValue: "=compareTo"
+                },
+                link: function (scope, element, attributes, ngModel) {
+
+                    ngModel.$validators.compareTo = function (modelValue) {
+                        return modelValue == scope.otherModelValue;
+                    };
+
+                    scope.$watch("otherModelValue", function () {
+                        ngModel.$validate();
+                    });
+                }
+            };
+        };
     })
 ;
