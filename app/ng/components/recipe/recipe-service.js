63a6bea26014eb6e64445b13b6c4133ea05e25dc
Simon Bönisch <simonboenisch@yahoo.de>
2016-07-02 22:30:14 +0200

    implemented getIngredientList service

--- app/ng/components/recipe/recipe-service.js
+++ app/ng/components/recipe/recipe-service.js
@@ -7,6 +7,7 @@
 
         this.get = getRecipe;
         this.getVariations = getVariations;
+        this.getIngredients = getIngredients;
 
         ////////////////
 
@@ -18,6 +19,21 @@
         function getVariations(recipeId) {
             console.log("Loading variations for recipe with id '" + recipeId + "'.");
             return $http.get(BASEURL + '/recipes/recipeFamily/' + recipeId);
+        }
+
+        function getIngredients(ingredientListId) {
+            console.log("Loading ingredient list with id '" + ingredientListId + "'.");
+            var ingredients = [];
+            $http.get(BASEURL + '/ingredientLists/' + ingredientListId).then(function (listResponse) {
+                var ingredientList = listResponse.data.ingredients;
+                ingredientList.forEach(function (ingredient) {
+                    $http.get(BASEURL + '/ingredients/' + ingredient.ingredient).then(function (ingredientResponse) {
+                        ingredient['title'] = ingredientResponse.data.title;
+                    });
+                    ingredients.push(ingredient);
+                });
+            });
+            return ingredients;
         }
 
     }
