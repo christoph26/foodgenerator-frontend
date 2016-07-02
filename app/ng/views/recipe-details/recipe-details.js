6c3bccd89c865de78127d77b9822ba588a52f74a
Simon Bönisch <simonboenisch@yahoo.de>
2016-07-02 22:42:06 +0200

    fixed 404 request when recipe id undefined

--- app/ng/views/recipe-details/recipe-details.js
+++ app/ng/views/recipe-details/recipe-details.js
@@ -17,23 +17,25 @@ angular.module('foodGenerator.recipeDetail', ['ngResource', 'ui.router'])
     .controller('RecipeDetailCtrl', function ($scope, $stateParams, $http, recipeStorageService, recipeService, BASEURL) {
         $scope.person = 1;
 
-        recipeService.get($stateParams.recipeId).then(function (response) {
-            // load the recipe itself
-            $scope.recipe = response.data;
+        if ($stateParams.recipeId) {
+            recipeService.get($stateParams.recipeId).then(function (response) {
+                // load the recipe itself
+                $scope.recipe = response.data;
 
-            // load the ingredient list
-            $scope.recipe.ingredientList = recipeService.getIngredients($scope.recipe.ingredientList);
+                // load the ingredient list
+                $scope.recipe.ingredientList = recipeService.getIngredients($scope.recipe.ingredientList);
 
-            // load the recipe variations
-            recipeService.getVariations($scope.recipe._id).then(function (response) {
-                $scope.recipeVariations = response.data;
-            }, function () {
-                $scope.recipeVariations = [];
+                // load the recipe variations
+                recipeService.getVariations($scope.recipe._id).then(function (response) {
+                    $scope.recipeVariations = response.data;
+                }, function () {
+                    $scope.recipeVariations = [];
+                });
+
+                // track that recipe was viewed
+                recipeStorageService.addRecentlyViewedRecipe($scope.recipe);
             });
-
-            // track that recipe was viewed
-            recipeStorageService.addRecentlyViewedRecipe($scope.recipe);
-        });
+        }
 
         $scope.addToMealPlanner = addToMealPlanner;
         $scope.addToShoppingList = addToShoppingList;
