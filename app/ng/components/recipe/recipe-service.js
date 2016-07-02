a47750628b9eec9530ea0c9eeb50dd5a1f8c0dae
Simon Bönisch <simonboenisch@yahoo.de>
2016-07-02 21:27:14 +0200

    extended recipe service by a getVariations method

--- app/ng/components/recipe/recipe-service.js
+++ app/ng/components/recipe/recipe-service.js
@@ -6,12 +6,18 @@
     function recipeService(BASEURL, $http) {
 
         this.get = getRecipe;
+        this.getVariations = getVariations;
 
         ////////////////
 
         function getRecipe(recipeId) {
             console.log("Loading recipe with id '" + recipeId + "'.");
             return $http.get(BASEURL + '/recipes/' + recipeId);
+        }
+
+        function getVariations(recipeId) {
+            console.log("Loading variations for recipe with id '" + recipeId + "'.");
+            return $http.get(BASEURL + '/recipes/recipeFamily/' + recipeId);
         }
 
     }
