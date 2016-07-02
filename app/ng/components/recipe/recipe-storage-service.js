1dafb2d11e39203162dfdf7199af91d53768852e
Simon Bönisch <simonboenisch@yahoo.de>
2016-07-02 19:53:19 +0200

    extended recipe storage service to feature a shopping list storage

--- app/ng/components/recipe/recipe-storage-service.js
+++ app/ng/components/recipe/recipe-storage-service.js
@@ -7,12 +7,21 @@
 
         this.addMarkedRecipe = addMarkedRecipe;
         this.removeMarkedRecipe = removeMarkedRecipe;
-        this.addRecentlyViewedRecipe = addRecentlyViewedRecipe;
-
         this.getMarkedRecipeIds = getMarkedRecipeIds;
+        this.clearMarkedRecipes = clearMarkedRecipes;
+
+        this.addRecentlyViewedRecipe = addRecentlyViewedRecipe;
         this.getRecentlyViewedRecipeIds = getRecentlyViewedRecipeIds;
+        this.clearRecentlyViewedRecipes = clearRecentlyViewedRecipes;
+
+        this.addToShoppingList = addToShoppingList;
+        this.removeFromShoppingList = removeFromShoppingList;
+        this.getShoppingList = getShoppingList;
+        this.clearShoppingList = clearShoppingList;
 
         ////////////////
+
+        // Marked Recipes
 
         function addMarkedRecipe(recipe) {
             console.log("Adding recipe '" + recipe.title + "' to marked recipes.");
@@ -31,6 +40,12 @@
             return $window.localStorage['foodgenerator.recipes.marked'];
         }
 
+        function clearMarkedRecipes() {
+            $window.localStorage.removeItem('foodgenerator.recipes.marked');
+        }
+
+        // Recently Viewed Recipes
+
         function addRecentlyViewedRecipe(recipe) {
             console.log("Registering new view for recipe '" + recipe.title + "'.");
             addRecipeToStorage(recipe, 'foodgenerator.recipes.recent');
@@ -39,6 +54,35 @@
         function getRecentlyViewedRecipeIds() {
             return $window.localStorage['foodgenerator.recipes.recent'];
         }
+
+        function clearRecentlyViewedRecipes() {
+            $window.localStorage.removeItem('foodgenerator.recipes.marked');
+        }
+
+        // Shopping List
+
+        function addToShoppingList(recipe) {
+            console.log("Adding recipe '" + recipe.title + "' to shopping list.");
+            addRecipeToStorage(recipe, 'foodgenerator.recipes.shopping');
+        }
+
+        function removeFromShoppingList(recipe) {
+            if (removeRecipeFromStorage(recipe, 'foodgenerator.recipes.shopping')) {
+                console.log("Removed recipe '" + recipe.title + "' from shopping list.");
+            } else {
+                console.error("Could not remove recipe '" + recipe.title + "': not contained in shopping list.");
+            }
+        }
+
+        function getShoppingList() {
+            return $window.localStorage['foodgenerator.recipes.shopping'];
+        }
+
+        function clearShoppingList() {
+            $window.localStorage.removeItem('foodgenerator.recipes.shopping');
+        }
+
+        // Generic Functions
 
         function addRecipeToStorage(recipe, storageName) {
             // in case it is contained, remove element from current position
