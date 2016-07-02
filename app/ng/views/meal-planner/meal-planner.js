b3e3cc83f156b0a5b7a4ec1686909f0d7d2b5f4e
Simon Bönisch <simonboenisch@yahoo.de>
2016-07-02 20:51:58 +0200

    integrated recipeStorageService into meal planner; removed example data

--- app/ng/views/meal-planner/meal-planner.js
+++ app/ng/views/meal-planner/meal-planner.js
@@ -14,12 +14,12 @@ angular.module('foodGenerator.mealPlanner', ['ngResource', 'ui.router', 'dndList
             })
     })
 
-    .controller('MealPlannerCtrl', function ($scope, $filter, $uibModal, currentUser, mealPlanService) {
+    .controller('MealPlannerCtrl', function ($scope, $filter, $uibModal, currentUser, recipeStorageService, mealPlanService) {
         resetMealPlan();
 
         $scope.loggedIn = false;
-        $scope.markedRecipes = markedExampleRecipes;
-        $scope.recentRecipes = viewedExampleRecipes;
+        $scope.markedRecipes = recipeStorageService.getMarkedRecipes();
+        $scope.recentRecipes = recipeStorageService.getRecentlyViewedRecipes();
 
         $scope.open = open;
 
@@ -125,6 +125,7 @@ angular.module('foodGenerator.mealPlanner', ['ngResource', 'ui.router', 'dndList
 
         function removeMarkedRecipeCallback(recipe) {
             removeRecipeFromList(recipe, $scope.markedRecipes);
+            recipeStorageService.removeMarkedRecipe(recipe);
         }
 
         ////////////////////////
@@ -177,64 +178,3 @@ angular.module('foodGenerator.mealPlanner', ['ngResource', 'ui.router', 'dndList
 
     })
 ;
-
-var markedExampleRecipes = [
-    {
-        "_id": "333300000000000000000003",
-        "title": "Chili Con Carne",
-        "effort": 1,
-        "description": "Hot & Spicy",
-        "vegan": false,
-        "vegetarian": false,
-        "picture": "img/recipes/chili-con-carne.jpg",
-        "ingredientList": "222200000000000000000003",
-        "recipeFamily": "444400000000000000000002"
-    },
-    {
-        "_id": "333300000000000000000002",
-        "title": "Rammstein Spaghetti Bolognese",
-        "effort": 3,
-        "description": "HOT as Hell.",
-        "vegan": false,
-        "vegetarian": false,
-        "picture": "img/recipes/spaghetti-bolognese-rammstein.jpg",
-        "ingredientList": "222200000000000000000002",
-        "recipeFamily": "444400000000000000000001"
-    }
-];
-
-var viewedExampleRecipes = [
-    {
-        "_id": "333300000000000000000001",
-        "title": "Spaghetti Bolognese Classic",
-        "effort": 1,
-        "description": "Preperation is simple and done in 30 minutes.",
-        "vegan": false,
-        "vegetarian": false,
-        "picture": "img/recipes/spaghetti-bolognese-classic.jpg",
-        "ingredientList": "222200000000000000000001",
-        "recipeFamily": "444400000000000000000001"
-    },
-    {
-        "_id": "333300000000000000000004",
-        "title": "Avocado Salad",
-        "effort": 1,
-        "description": "Delicious Healthy Salat for Vegan People",
-        "vegan": true,
-        "vegetarian": true,
-        "picture": "img/recipes/avocado-salad.jpg",
-        "ingredientList": "222200000000000000000004",
-        "recipeFamily": "444400000000000000000003"
-    },
-    {
-        "_id": "333300000000000000000005",
-        "title": "Avocado Salad Feta",
-        "effort": 1,
-        "description": "Delicious Healthy Salat for Vegetarian People",
-        "vegan": false,
-        "vegetarian": true,
-        "picture": "img/recipes/avocado-salad-feta.jpg",
-        "ingredientList": "222200000000000000000005",
-        "recipeFamily": "444400000000000000000003"
-    }
-];
